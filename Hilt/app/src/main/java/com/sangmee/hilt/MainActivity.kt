f0088129ee7d6c45dd1aee4f7460ced1b5616e47
sangminkim <apfhdznzl@gmail.com>
2020-10-29 21:52:09 +0900

    AndroidEntryPoint, Inject 어노테이션 사용

--- Hilt/app/src/main/java/com/sangmee/hilt/MainActivity.kt
+++ Hilt/app/src/main/java/com/sangmee/hilt/MainActivity.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sangmee.hilt.data.local.LocalDataSourceImpl
 import com.sangmee.hilt.data.remote.RemoteDataSourceImpl
+import com.sangmee.hilt.data.repository.Repository
 import com.sangmee.hilt.data.repository.RepositoryImpl
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel by lazy {
-        val localDataSource = LocalDataSourceImpl()
-        val remoteDataSource = RemoteDataSourceImpl()
-        val repository = RepositoryImpl(localDataSource, remoteDataSource)
-        MainViewModel(repository)
-    }
+//    private val mainViewModel by lazy {
+//        val localDataSource = LocalDataSourceImpl()
+//        val remoteDataSource = RemoteDataSourceImpl()
+//        val repository = RepositoryImpl(localDataSource, remoteDataSource)
+//        MainViewModel(repository)
+//    }
+
+    //Inject 어노테이션 : 필드에 의존성 주입
+    @Inject lateinit var repository: Repository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_output.text = mainViewModel.result
+//        tv_output.text = mainViewModel.result
+        tv_output.text = repository.getData()
     }
 }
 
